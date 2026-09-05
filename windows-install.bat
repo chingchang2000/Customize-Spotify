b298ca3fd64d23d7fa7c3c130367da46389ff707
JensSkur <140887443+chingchang2000@users.noreply.github.com>
2026-09-05 14:46:14 +0200

    Redirect old installer to one-click setup

--- windows-install.bat
+++ windows-install.bat
@@ -1,14 +1,3 @@
 @echo off
-title Customify Installer
 cd /d "%~dp0"
-where node >nul 2>nul || (
-  echo Node.js is required. Install Node.js 20+ from https://nodejs.org/
-  pause
-  exit /b 1
-)
-echo Installing Customify...
-call npm install
-if errorlevel 1 pause & exit /b 1
-echo.
-echo Done. Starting Customify...
-call npm start
+call INSTALL-CUSTOMIFY.bat
